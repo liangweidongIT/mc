06f2323440e65dc878c588d8d225b774ed92d117
liangwd_local <liangweidongIT@163.com>
2018-12-10 12:00:37 +0800

    本地第3次提交

--- src/main/java/com/demo/mc/AppStart.java
+++ src/main/java/com/demo/mc/AppStart.java
@@ -15,5 +15,6 @@ public class AppStart {
         log.info("update from local_111111");
         log.info("update from local_222222");
         log.info("update form git_1111111");
+        log.info("update form local_333333");
     }
 }
