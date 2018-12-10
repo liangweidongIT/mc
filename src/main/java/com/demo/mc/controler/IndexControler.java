19dfc9a4e19e58d5dce5249657fe6f89845b214a
liangwd_local <liangweidongIT@163.com>
2018-12-10 14:07:46 +0800

    本地第master_index_3次提交

--- src/main/java/com/demo/mc/controler/IndexControler.java
+++ src/main/java/com/demo/mc/controler/IndexControler.java
@@ -1,5 +1,6 @@
 package com.demo.mc.controler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by lenovo on 2018/12/10.
  */
 @RestController
+@Slf4j
 public class IndexControler {
 
     @RequestMapping(value="index")
     public String index() {
+        log.info("index_111");
        return "hello index";
     }
 }
