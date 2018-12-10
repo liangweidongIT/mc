fbdbb8aeee9b16e5b0172dfaf3f895ecda8cea07
liangwd_local <liangweidongIT@163.com>
2018-12-10 11:39:47 +0800

    本地第一次提交

---
+++ src/main/java/com/demo/mc/AppStart.java
@@ -0,0 +1,15 @@
+package com.demo.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lenovo on 2018/12/10.
+ */
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+
+    }
+}
