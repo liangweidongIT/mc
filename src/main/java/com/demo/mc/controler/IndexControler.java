795611a80f31f29a46a9b82314c79d3d4a109c29
lwd <liangweidongIT@163.com>
2018-12-20 21:27:16 +0800

    test_b1_b1提交

--- src/main/java/com/demo/mc/controler/IndexControler.java
+++ src/main/java/com/demo/mc/controler/IndexControler.java
@@ -18,6 +18,7 @@ public class IndexControler {
         log.info("update from local");
         log.info("update from github");
         log.info("update from test_b1");
+        log.info("update from test_b1_b1");
        return "jenkins 终于整好了22222222222222222222222";
     }
 }
